dd055716a73c3c8d306597e864aa33e9fbd8d7f4
alulab14 <alulab14@inf.pucp.edu.pe>
2018-10-26 12:50:00 -0500

    avances

--- TrabajoJavaLp2/src/Vista/frmPanel.java
+++ TrabajoJavaLp2/src/Vista/frmPanel.java
@@ -5,6 +5,10 @@
  */
 package Vista;
 
+import java.awt.Graphics;
+import java.awt.Image;
+import javax.swing.ImageIcon;
+
 /**
  *
  * @author alulab14
@@ -23,9 +27,7 @@ public class frmPanel extends javax.swing.JDialog {
         panel1.setVisible(false);
         panel2.setVisible(false);
         panel3.setVisible(false);
-        panel4.setVisible(false);
         panel5.setVisible(false);
-        panel7.setVisible(false);
                 
     }
 
@@ -42,40 +44,32 @@ public class frmPanel extends javax.swing.JDialog {
         panel0 = new javax.swing.JPanel();
         jButton12 = new javax.swing.JButton();
         jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
         jButton3 = new javax.swing.JButton();
-        jButton4 = new javax.swing.JButton();
         jButton10 = new javax.swing.JButton();
         panel1 = new javax.swing.JPanel();
         jButton5 = new javax.swing.JButton();
         jButton6 = new javax.swing.JButton();
-        jButton7 = new javax.swing.JButton();
         jButton8 = new javax.swing.JButton();
-        jButton9 = new javax.swing.JButton();
         jButton11 = new javax.swing.JButton();
         panel2 = new javax.swing.JPanel();
         jButton13 = new javax.swing.JButton();
-        jButton14 = new javax.swing.JButton();
-        jButton15 = new javax.swing.JButton();
-        jButton16 = new javax.swing.JButton();
         jButton17 = new javax.swing.JButton();
-        panel7 = new javax.swing.JPanel();
-        jButton19 = new javax.swing.JButton();
-        jButton18 = new javax.swing.JButton();
         panel3 = new javax.swing.JPanel();
         jButton23 = new javax.swing.JButton();
         jButton24 = new javax.swing.JButton();
         jButton25 = new javax.swing.JButton();
-        panel4 = new javax.swing.JPanel();
-        jButton26 = new javax.swing.JButton();
-        jButton27 = new javax.swing.JButton();
-        jButton28 = new javax.swing.JButton();
         panel5 = new javax.swing.JPanel();
         jButton29 = new javax.swing.JButton();
         jButton30 = new javax.swing.JButton();
         jButton31 = new javax.swing.JButton();
         jButton32 = new javax.swing.JButton();
-        desktop = new javax.swing.JDesktopPane();
+        ImageIcon icon=new ImageIcon(getClass().getResource("/Vista/png3/fondo.jpg"));
+        Image image = icon.getImage();
+        desktop = new javax.swing.JDesktopPane(){
+            public void paintComponent(Graphics g){
+                g.drawImage(image,0,0,getWidth(),getHeight(),this);
+            }
+        };
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         setTitle("Menu");
@@ -84,7 +78,7 @@ public class frmPanel extends javax.swing.JDialog {
         setSize(new java.awt.Dimension(980, 400));
         getContentPane().setLayout(null);
 
-        jPanel1.setBackground(new java.awt.Color(204, 204, 204));
+        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
         jPanel1.setLayout(null);
 
         panel0.setBackground(new java.awt.Color(0, 122, 204));
@@ -107,25 +101,18 @@ public class frmPanel extends javax.swing.JDialog {
         jButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Vista/png3/004-code.png"))); // NOI18N
         jButton1.setBorder(null);
         jButton1.setContentAreaFilled(false);
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
         panel0.add(jButton1);
-
-        jButton2.setBackground(new java.awt.Color(0, 122, 204));
-        jButton2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Vista/png3/005-box.png"))); // NOI18N
-        jButton2.setBorder(null);
-        jButton2.setContentAreaFilled(false);
-        panel0.add(jButton2);
 
         jButton3.setBackground(new java.awt.Color(0, 122, 204));
         jButton3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Vista/png3/002-shop.png"))); // NOI18N
         jButton3.setBorder(null);
         jButton3.setContentAreaFilled(false);
         panel0.add(jButton3);
-
-        jButton4.setBackground(new java.awt.Color(0, 122, 204));
-        jButton4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Vista/png3/007-cash.png"))); // NOI18N
-        jButton4.setBorder(null);
-        jButton4.setContentAreaFilled(false);
-        panel0.add(jButton4);
 
         jButton10.setBackground(new java.awt.Color(0, 122, 204));
         jButton10.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Vista/png3/003-report.png"))); // NOI18N
@@ -159,19 +146,6 @@ public class frmPanel extends javax.swing.JDialog {
         });
         panel1.add(jButton6);
 
-        jButton7.setBackground(new java.awt.Color(0, 122, 204));
-        jButton7.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
-        jButton7.setForeground(new java.awt.Color(255, 255, 255));
-        jButton7.setText("Pedido");
-        jButton7.setBorder(javax.swing.BorderFactory.createEtchedBorder());
-        jButton7.setContentAreaFilled(false);
-        jButton7.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton7ActionPerformed(evt);
-            }
-        });
-        panel1.add(jButton7);
-
         jButton8.setBackground(new java.awt.Color(0, 122, 204));
         jButton8.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
         jButton8.setForeground(new java.awt.Color(255, 255, 255));
@@ -184,20 +158,6 @@ public class frmPanel extends javax.swing.JDialog {
             }
         });
         panel1.add(jButton8);
-
-        jButton9.setBackground(new java.awt.Color(0, 122, 204));
-        jButton9.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
-        jButton9.setForeground(new java.awt.Color(255, 255, 255));
-        jButton9.setText("Pagos");
-        jButton9.setBorder(javax.swing.BorderFactory.createEtchedBorder());
-        jButton9.setContentAreaFilled(false);
-        jButton9.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
-        jButton9.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton9ActionPerformed(evt);
-            }
-        });
-        panel1.add(jButton9);
 
         jButton11.setBackground(new java.awt.Color(0, 122, 204));
         jButton11.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
@@ -225,31 +185,6 @@ public class frmPanel extends javax.swing.JDialog {
         jButton13.setContentAreaFilled(false);
         panel2.add(jButton13);
 
-        jButton14.setBackground(new java.awt.Color(0, 122, 204));
-        jButton14.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
-        jButton14.setForeground(new java.awt.Color(255, 255, 255));
-        jButton14.setText("Usuarios");
-        jButton14.setBorder(javax.swing.BorderFactory.createEtchedBorder());
-        jButton14.setContentAreaFilled(false);
-        panel2.add(jButton14);
-
-        jButton15.setBackground(new java.awt.Color(0, 122, 204));
-        jButton15.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
-        jButton15.setForeground(new java.awt.Color(255, 255, 255));
-        jButton15.setText("Clientes");
-        jButton15.setBorder(javax.swing.BorderFactory.createEtchedBorder());
-        jButton15.setContentAreaFilled(false);
-        panel2.add(jButton15);
-
-        jButton16.setBackground(new java.awt.Color(0, 122, 204));
-        jButton16.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
-        jButton16.setForeground(new java.awt.Color(255, 255, 255));
-        jButton16.setText("Transportistas");
-        jButton16.setToolTipText("");
-        jButton16.setBorder(javax.swing.BorderFactory.createEtchedBorder());
-        jButton16.setContentAreaFilled(false);
-        panel2.add(jButton16);
-
         jButton17.setBackground(new java.awt.Color(0, 122, 204));
         jButton17.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
         jButton17.setForeground(new java.awt.Color(255, 255, 255));
@@ -264,27 +199,7 @@ public class frmPanel extends javax.swing.JDialog {
         panel2.add(jButton17);
 
         jPanel1.add(panel2);
-        panel2.setBounds(180, 0, 130, 400);
-
-        panel7.setBackground(new java.awt.Color(0, 122, 204));
-        panel7.setLayout(new java.awt.GridLayout(10, 1));
-
-        jButton19.setBackground(new java.awt.Color(0, 122, 204));
-        jButton19.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
-        jButton19.setBorderPainted(false);
-        jButton19.setContentAreaFilled(false);
-        panel7.add(jButton19);
-
-        jButton18.setBackground(new java.awt.Color(0, 122, 204));
-        jButton18.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
-        jButton18.setForeground(new java.awt.Color(255, 255, 255));
-        jButton18.setText("Vendedores");
-        jButton18.setBorder(javax.swing.BorderFactory.createEtchedBorder());
-        jButton18.setContentAreaFilled(false);
-        panel7.add(jButton18);
-
-        jPanel1.add(panel7);
-        panel7.setBounds(310, 0, 130, 400);
+        panel2.setBounds(180, 0, 150, 400);
 
         panel3.setBackground(new java.awt.Color(0, 122, 204));
         panel3.setLayout(new java.awt.GridLayout(10, 1));
@@ -312,35 +227,7 @@ public class frmPanel extends javax.swing.JDialog {
         panel3.add(jButton25);
 
         jPanel1.add(panel3);
-        panel3.setBounds(440, 0, 130, 400);
-
-        panel4.setBackground(new java.awt.Color(0, 122, 204));
-        panel4.setLayout(new java.awt.GridLayout(10, 1));
-
-        jButton26.setBackground(new java.awt.Color(0, 122, 204));
-        jButton26.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
-        jButton26.setBorderPainted(false);
-        jButton26.setContentAreaFilled(false);
-        panel4.add(jButton26);
-
-        jButton27.setBackground(new java.awt.Color(0, 122, 204));
-        jButton27.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
-        jButton27.setForeground(new java.awt.Color(255, 255, 255));
-        jButton27.setText("Factura");
-        jButton27.setBorder(javax.swing.BorderFactory.createEtchedBorder());
-        jButton27.setContentAreaFilled(false);
-        panel4.add(jButton27);
-
-        jButton28.setBackground(new java.awt.Color(0, 122, 204));
-        jButton28.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
-        jButton28.setForeground(new java.awt.Color(255, 255, 255));
-        jButton28.setText("Gestion de Pedidos");
-        jButton28.setBorder(javax.swing.BorderFactory.createEtchedBorder());
-        jButton28.setContentAreaFilled(false);
-        panel4.add(jButton28);
-
-        jPanel1.add(panel4);
-        panel4.setBounds(570, 0, 130, 400);
+        panel3.setBounds(440, 0, 150, 400);
 
         panel5.setBackground(new java.awt.Color(0, 122, 204));
         panel5.setLayout(new java.awt.GridLayout(10, 1));
@@ -393,11 +280,14 @@ public class frmPanel extends javax.swing.JDialog {
         jPanel1.add(panel5);
         panel5.setBounds(700, 0, 150, 400);
 
+        desktop.setBackground(new java.awt.Color(255, 255, 255));
+        desktop.setForeground(new java.awt.Color(255, 255, 255));
+
         javax.swing.GroupLayout desktopLayout = new javax.swing.GroupLayout(desktop);
         desktop.setLayout(desktopLayout);
         desktopLayout.setHorizontalGroup(
             desktopLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 930, Short.MAX_VALUE)
+            .addGap(0, 650, Short.MAX_VALUE)
         );
         desktopLayout.setVerticalGroup(
             desktopLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -405,7 +295,7 @@ public class frmPanel extends javax.swing.JDialog {
         );
 
         jPanel1.add(desktop);
-        desktop.setBounds(50, 0, 930, 400);
+        desktop.setBounds(330, 0, 650, 400);
 
         getContentPane().add(jPanel1);
         jPanel1.setBounds(0, 0, 980, 400);
@@ -421,9 +311,7 @@ public class frmPanel extends javax.swing.JDialog {
             panel1.setVisible(false);
             panel2.setVisible(false);
             panel3.setVisible(false);
-            panel4.setVisible(false);
             panel5.setVisible(false);
-            panel7.setVisible(false);
         }
     }//GEN-LAST:event_jButton12ActionPerformed
 
@@ -432,29 +320,12 @@ public class frmPanel extends javax.swing.JDialog {
         if(panel2.isVisible()==false){
             panel2.setVisible(true);
             panel3.setVisible(false);
-            panel4.setVisible(false);
             panel5.setVisible(false);
-            panel7.setVisible(false);
         }
         else{
             panel2.setVisible(false);
         }
     }//GEN-LAST:event_jButton6ActionPerformed
-
-    private void jButton7ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton7ActionPerformed
-        // TODO add your handling code here:
-        if(panel4.isVisible()==false){
-            panel4.setVisible(true);
-            panel4.setLocation(180,0);
-            panel2.setVisible(false);
-            panel3.setVisible(false);
-            panel5.setVisible(false);
-            panel7.setVisible(false);
-        }
-        else{
-            panel4.setVisible(false);
-        }
-    }//GEN-LAST:event_jButton7ActionPerformed
 
     private void jButton8ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton8ActionPerformed
         // TODO add your handling code here:
@@ -463,28 +334,11 @@ public class frmPanel extends javax.swing.JDialog {
             panel5.setLocation(180,0);
             panel2.setVisible(false);
             panel3.setVisible(false);
-            panel4.setVisible(false);
-            panel7.setVisible(false);
         }
         else{
             panel5.setVisible(false);
         }
     }//GEN-LAST:event_jButton8ActionPerformed
-
-    private void jButton9ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton9ActionPerformed
-        // TODO add your handling code here:
-        if(panel7.isVisible()==false){
-            panel7.setVisible(true);
-            panel7.setLocation(180,0);
-            panel2.setVisible(false);
-            panel3.setVisible(false);
-            panel4.setVisible(false);
-            panel5.setVisible(false);
-        }
-        else{
-            panel7.setVisible(false);
-        }
-    }//GEN-LAST:event_jButton9ActionPerformed
 
     private void jButton11ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton11ActionPerformed
         // TODO add your handling code here:
@@ -492,9 +346,7 @@ public class frmPanel extends javax.swing.JDialog {
             panel3.setVisible(true);
             panel3.setLocation(180,0);
             panel2.setVisible(false);
-            panel4.setVisible(false);
             panel5.setVisible(false);
-            panel7.setVisible(false);
         }
         else{
             panel3.setVisible(false);
@@ -519,8 +371,12 @@ public class frmPanel extends javax.swing.JDialog {
 
     private void jButton31ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton31ActionPerformed
         // TODO add your handling code here:
-        frmGestionAlmacen nuevo = new frmGestionAlmacen(null,true);
+        frmGestAlma nuevo = new frmGestAlma();
+        desktop.add(nuevo);
         nuevo.setVisible(true);
+        nuevo.setAlignmentX(TOP_ALIGNMENT);
+        nuevo.setSize(650,400);
+        nuevo.setAlignmentY(0);
     }//GEN-LAST:event_jButton31ActionPerformed
 
     private void jButton32ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton32ActionPerformed
@@ -528,6 +384,25 @@ public class frmPanel extends javax.swing.JDialog {
         fmrMovimientoAlmacen nuevo=new fmrMovimientoAlmacen(null,true);
         nuevo.setVisible(true);
     }//GEN-LAST:event_jButton32ActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+        if(panel1.isVisible()==false){
+            panel1.setVisible(true);
+            panel2.setVisible(true);
+        }
+        else{
+            if(panel1.isVisible()==true && panel2.isVisible()==false){
+                panel2.setVisible(true);
+            }
+            else{
+                panel1.setVisible(false);
+                panel2.setVisible(false);
+                panel3.setVisible(false);
+                panel5.setVisible(false);
+            }
+        }                                   
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -579,37 +454,23 @@ public class frmPanel extends javax.swing.JDialog {
     private javax.swing.JButton jButton11;
     private javax.swing.JButton jButton12;
     private javax.swing.JButton jButton13;
-    private javax.swing.JButton jButton14;
-    private javax.swing.JButton jButton15;
-    private javax.swing.JButton jButton16;
     private javax.swing.JButton jButton17;
-    private javax.swing.JButton jButton18;
-    private javax.swing.JButton jButton19;
-    private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton23;
     private javax.swing.JButton jButton24;
     private javax.swing.JButton jButton25;
-    private javax.swing.JButton jButton26;
-    private javax.swing.JButton jButton27;
-    private javax.swing.JButton jButton28;
     private javax.swing.JButton jButton29;
     private javax.swing.JButton jButton3;
     private javax.swing.JButton jButton30;
     private javax.swing.JButton jButton31;
     private javax.swing.JButton jButton32;
-    private javax.swing.JButton jButton4;
     private javax.swing.JButton jButton5;
     private javax.swing.JButton jButton6;
-    private javax.swing.JButton jButton7;
     private javax.swing.JButton jButton8;
-    private javax.swing.JButton jButton9;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel panel0;
     private javax.swing.JPanel panel1;
     private javax.swing.JPanel panel2;
     private javax.swing.JPanel panel3;
-    private javax.swing.JPanel panel4;
     private javax.swing.JPanel panel5;
-    private javax.swing.JPanel panel7;
     // End of variables declaration//GEN-END:variables
 }
